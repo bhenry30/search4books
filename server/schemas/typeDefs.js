c79f0d9e5316d5a69576f6b5ba7cddecbe0976c3
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-06-05 14:05:21 -0500

    adds typeDefs for user

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,31 @@
+const {gql} = require('apollo-server-express')
+
+const typeDefs = gql `
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: String
+    author: String
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type Query {
+    user: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+}
+`
+
+module.exports = typeDefs
